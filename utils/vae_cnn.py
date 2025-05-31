46d528846c387b0bd7cf858e5f043023b2d55255
Yoga-Syahputra <yogasyahputrauzumaki@gmail.com>
2025-05-31 12:15:32 +0700

    fix: readjust architecture

--- utils/vae_cnn.py
+++ utils/vae_cnn.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class VAE_CNN(nn.Module):
-    def __init__(self, latent_dim=4):
+    def __init__(self, latent_dim=8):
         super(VAE_CNN, self).__init__()
+
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(32),
@@ -16,33 +17,45 @@ class VAE_CNN(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
-        self.flattened_size = 128 * 12 * 10
+
+        # Output size (CNN)
+        self.flattened_size = 128 * 12 * 10 
+
         self.fc_mu = nn.Linear(self.flattened_size, latent_dim)
         self.fc_logvar = nn.Linear(self.flattened_size, latent_dim)
         self.fc_decode = nn.Linear(latent_dim, self.flattened_size)
+
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # [64,24,20]
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),   # [32,48,40]
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1, output_padding=(0, 1)),
+            nn.ConvTranspose2d(32, 1, kernel_size=(5,8), stride=2, padding=0, output_padding=(1,1)),  # [1,100,87]
         )
 
     def encode(self, x):
-        x = self.encoder(x).view(x.size(0), -1)
-        return self.fc_mu(x), self.fc_logvar(x)
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
-        return mu + std * torch.randn_like(std)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
     def decode(self, z):
-        x = self.fc_decode(z).view(-1, 128, 12, 10)
-        return F.interpolate(self.decoder(x), size=(100, 87), mode='bilinear', align_corners=False)
+      x = self.fc_decode(z)
+      x = x.view(-1, 128, 12, 10)
+      x = self.decoder(x)
+      return x
 
     def forward(self, x):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        recon_x = self.decode(z)
+        return recon_x, mu, logvar
+
